295697d792b3b75d66acba126fff1a9bebb85c5f
YanisAF <yanissf2@gmail.com>
2026-03-23 21:39:05 +0100

    feat(dockerCompose): configure docker compose and dockerfile

--- src/main/java/com/example/CESIZen/CesiZenApplication.java
+++ src/main/java/com/example/CESIZen/CesiZenApplication.java
@@ -2,12 +2,12 @@ package com.example.CESIZen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CesiZenApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(CesiZenApplication.class, args);
+        SpringApplication.run(CesiZenApplication.class, args);
 	}
-
 }
